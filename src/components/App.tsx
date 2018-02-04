0962d990934442380308862552d7e391ccc3d040
Horus Lugo <horusgoul@gmail.com>
2018-02-04 19:07:46 +0100

    Theme tweaks.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
+
 import MainMenu from "./main-menu/MainMenu";
+import TestSelection from "./test-selection/TestSelection";
 
 import "./App.scss";
 
@@ -11,6 +13,7 @@ class App extends React.Component<{}, {}> {
         <div className="app__content">
           <Switch>
             <Route exact={true} path="/" component={MainMenu} />
+            <Route exact={true} path="/tests" component={TestSelection} />
           </Switch>
         </div>
       </div>
---
+++ src/components/test-selection/TestSelection.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import "./TestSelection.scss";
+
+class TestSelection extends React.Component<{}, {}> {
+  public render() {
+    return <div className="test-selection">TestSelection</div>;
+  }
+}
+
+export default TestSelection;
