9c8810b9646cfc4334b0a557de22bf4859e9dbf6
Horus Lugo <horusgoul@gmail.com>
2018-02-06 20:44:31 +0100

    Adds navbar in valences test.

--- src/components/valences-test/ValencesTest.tsx
+++ src/components/valences-test/ValencesTest.tsx
@@ -1,11 +1,36 @@
+import autobind from "autobind-decorator";
 import * as React from "react";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import QuestionsTest from "../questions-test/QuestionsTest";
+import Navbar from "../shared/navbar/Navbar";
 
 import "./ValencesTest.scss";
 
-class ValencesTest extends React.Component<{}, {}> {
+type Props = RouteComponentProps<any> & React.Props<any>;
+
+@autobind
+class ValencesTest extends React.Component<Props, {}> {
   public render() {
-    return <div className="valences-test">ValencesTest</div>;
+    return (
+      <div className="valences-test">
+        <Navbar
+          title="Valences Test"
+          backButton={true}
+          onBackButtonClick={this.onNavbarBackButtonClick}
+        />
+
+        <div className="valences-test__test">
+          <QuestionsTest />
+        </div>
+      </div>
+    );
+  }
+
+  private onNavbarBackButtonClick() {
+    const { history } = this.props;
+
+    history.goBack();
   }
 }
 
-export default ValencesTest;
+export default withRouter<Props>(ValencesTest);
