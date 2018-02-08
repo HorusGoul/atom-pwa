80a53608920d5545366a806c83b32ca6384b2a37
Horus Lugo <horusgoul@gmail.com>
2018-02-08 23:05:52 +0100

    Adds state to question card answers.

--- src/components/questions-test/question-card/QuestionCard.tsx
+++ src/components/questions-test/question-card/QuestionCard.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 
 import Card from "../../shared/card/Card";
-import QuestionCardAnswer from "./question-card-answer/QuestionCardAnswer";
+import QuestionCardAnswer, {
+  IQuestionCardAnswer
+} from "./question-card-answer/QuestionCardAnswer";
 
 import "./QuestionCard.scss";
+
+interface IQuestionCard {
+  question: string;
+  answers: IQuestionCardAnswer[];
+}
 
 class QuestionCard extends React.Component<{}, {}> {
   public render() {
@@ -14,10 +21,10 @@ class QuestionCard extends React.Component<{}, {}> {
         <div className="question-card__question">Br</div>
 
         <div className="question-card__answers">
-          <QuestionCardAnswer>+1 +5</QuestionCardAnswer>
-          <QuestionCardAnswer>+1</QuestionCardAnswer>
-          <QuestionCardAnswer>+2 +4 +6 / -2</QuestionCardAnswer>
-          <QuestionCardAnswer>+1 +3 +5 +7 / -1</QuestionCardAnswer>
+          <QuestionCardAnswer answer="+1 +5" right={false} />
+          <QuestionCardAnswer answer="+1" right={false} />
+          <QuestionCardAnswer answer="+2 +4 +6 / -2" right={false} />
+          <QuestionCardAnswer answer="+1 +3 +5 +7 / -1" right={true} />
         </div>
       </Card>
     );
--- src/components/questions-test/question-card/question-card-answer/QuestionCardAnswer.tsx
+++ src/components/questions-test/question-card/question-card-answer/QuestionCardAnswer.tsx
@@ -1,17 +1,57 @@
+import autobind from "autobind-decorator";
+import * as classNames from "classnames";
 import * as React from "react";
 import Button, { IButtonProps } from "../../../shared/button/Button";
 
 import "./QuestionCardAnswer.scss";
 
-type Props = IButtonProps;
+export interface IQuestionCardAnswer {
+  answer: string;
+  right: boolean;
+}
 
-const QuestionCardAnswer: React.StatelessComponent<Props> = ({
-  onClick,
-  children
-}) => (
-  <Button className="question-card-answer" onClick={onClick}>
-    {children}
-  </Button>
-);
+type Props = IButtonProps & IQuestionCardAnswer;
+
+interface IQuestionCardAnswerState {
+  clicked: boolean;
+}
+
+@autobind
+class QuestionCardAnswer extends React.Component<
+  Props,
+  IQuestionCardAnswerState
+> {
+  public state: IQuestionCardAnswerState = {
+    clicked: false
+  };
+
+  public render() {
+    const { answer, right } = this.props;
+    const { clicked } = this.state;
+
+    const answerClass = classNames("question-card-answer", {
+      "question-card-answer--clicked": clicked,
+      "question-card-answer--wrong": clicked && !right
+    });
+
+    return (
+      <Button className={answerClass} onClick={this.onClick}>
+        {answer}
+      </Button>
+    );
+  }
+
+  private onClick() {
+    const { onClick } = this.props;
+
+    this.setState({
+      clicked: true
+    });
+
+    if (onClick) {
+      onClick();
+    }
+  }
+}
 
 export default QuestionCardAnswer;
