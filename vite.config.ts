62567ed0d992962a2f4b59ddadf7640dc7430650
Horus Lugo <horusgoul@gmail.com>
2021-01-25 01:04:22 +0100

    feat(build): move index.html and public folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import { defineConfig } from 'vite'
-import svgr from 'vite-plugin-svgr'
-import path from 'path'
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 export default defineConfig({
-  optimizeDeps: { include: ['firebase/app', 'firebase/analytics', '@material-ui/core/ButtonBase'], exclude: ['hammerjs'] },
+  optimizeDeps: {
+    include: [
+      "firebase/app",
+      "firebase/analytics",
+      "@material-ui/core/ButtonBase",
+    ],
+    exclude: ["hammerjs"],
+  },
   plugins: [reactRefresh(), svgr()],
   alias: {
-    '@': path.resolve('./src')
-  }
-})
+    "@": path.resolve("./src"),
+  },
+});
