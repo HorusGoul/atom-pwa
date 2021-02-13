61b0b59ed1e4bebfee4f4f5aa9f4923f95bc4618
Horus Lugo <horusgoul@gmail.com>
2021-02-14 00:30:00 +0100

    feat: remove gtag and AtomNativeFA typings

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,17 +1,9 @@
 interface Window {
-  dataLayer: any[];
   AtomNative: {
     isHybrid?: () => boolean;
     getDebugMode?: () => boolean;
     getSystemLanguage?: () => string | undefined;
   };
-  AtomNativeFA: {
-    logEvent?: (type: string, jsonParams: string) => void;
-    setUserProperty?: (name: string, value: string) => void;
-  };
-
-  // @ts-ignore
-  gtag: Gtag.Gtag;
 }
 
 declare module "*.svg" {
