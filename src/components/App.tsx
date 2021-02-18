4b3abd0ee2c7bb8e02044741eeb9f728105ee74c
Horus Lugo <horusgoul@gmail.com>
2021-02-19 00:21:20 +0100

    refactor(App): replace class component with a functional one

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,3 @@
-import autobind from "autobind-decorator";
 import * as React from "react";
 import { Helmet } from "react-helmet";
 import { Route, Switch } from "react-router-dom";
@@ -28,100 +27,92 @@ import TestSelection from "./test-selection/TestSelection";
 import ValencesTestSettings from "./valences-test/settings/ValencesTestSettings";
 import ValencesTest from "./valences-test/ValencesTest";
 
-@autobind
-class App extends React.Component<unknown> {
-  public componentDidMount() {
-    Locale.addLocaleChangeListener(this.reRenderApp);
-    Theme.addThemeChangeListener(this.setThemeClass);
-  }
+function App() {
+  const [, setUpdateKey] = React.useState(0);
+  const [theme, setTheme] = React.useState(() => Theme.getCurrentTheme());
 
-  public componentWillUnmount() {
-    Locale.removeLocaleChangeListener(this.reRenderApp);
-    Theme.removeThemeChangeListener(this.setThemeClass);
-  }
+  // TODO: Migrate Locale and Theme to use the Context API
+  React.useEffect(() => {
+    Locale.addLocaleChangeListener(forceUpdate);
+    Theme.addThemeChangeListener(setTheme);
 
-  public UNSAFE_componentWillMount() {
-    this.setThemeClass(Theme.getCurrentTheme());
-  }
+    function forceUpdate() {
+      setUpdateKey((current) => current + 1);
+    }
 
-  public render() {
-    const primaryColor = Theme.getPrimaryColor();
+    return () => {
+      Locale.removeLocaleChangeListener(forceUpdate);
+      Theme.removeThemeChangeListener(setTheme);
+    };
+  }, []);
 
-    return (
-      <div className="app">
-        <Helmet>
-          <html lang={Locale.getCurrentLang()} />
-
-          <link
-            rel="mask-icon"
-            href="/icons/safari-pinned-tab.svg"
-            color={primaryColor}
-          />
-          <meta name="msapplication-TileColor" content={primaryColor} />
-          <meta name="theme-color" content={primaryColor} />
-
-          <meta name="description" content={i18n("app_description")} />
-
-          <meta name="og:title" content={i18n("app_full_title")} />
-          <meta name="og:description" content={i18n("app_description")} />
-          <meta name="twitter:site" content={i18n("twitter_account")} />
-
-          <title>{i18n("app_full_title")}</title>
-        </Helmet>
-
-        <div className="app__content">
-          <Route path="/" component={SpycatSetup} />
-
-          <Switch>
-            <Route exact={true} path={MAIN_MENU} component={MainMenu} />
-            <Route
-              exact={true}
-              path={TEST_SELECTION}
-              component={TestSelection}
-            />
-            <Route exact={true} path={TEST_VALENCES} component={ValencesTest} />
-            <Route
-              exact={true}
-              path={TEST_VALENCES_SETTINGS}
-              component={ValencesTestSettings}
-            />
-            <Route
-              exact={true}
-              path={TEST_PERIODIC_TABLE}
-              component={PeriodicTableTest}
-            />
-            <Route
-              exact={true}
-              path={TEST_PERIODIC_TABLE_SETTINGS}
-              component={PeriodicTableTestSettings}
-            />
-            <Route
-              exact={true}
-              path={MASS_CALCULATOR}
-              component={MassCalculator}
-            />
-            <Route
-              exact={true}
-              path={PERIODIC_TABLE}
-              component={PeriodicTablePage}
-            />
-            <Route exact={true} path={ABOUT} component={About} />
-
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </div>
-    );
-  }
-
-  private setThemeClass(theme: string) {
+  React.useLayoutEffect(() => {
     document.body.className = "theme-" + theme;
-    this.reRenderApp();
-  }
+  }, [theme]);
 
-  private reRenderApp() {
-    this.forceUpdate();
-  }
+  const primaryColor = Theme.getPrimaryColor();
+
+  return (
+    <div className="app">
+      <Helmet>
+        <html lang={Locale.getCurrentLang()} />
+
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color={primaryColor}
+        />
+        <meta name="msapplication-TileColor" content={primaryColor} />
+        <meta name="theme-color" content={primaryColor} />
+
+        <meta name="description" content={i18n("app_description")} />
+
+        <meta name="og:title" content={i18n("app_full_title")} />
+        <meta name="og:description" content={i18n("app_description")} />
+        <meta name="twitter:site" content={i18n("twitter_account")} />
+
+        <title>{i18n("app_full_title")}</title>
+      </Helmet>
+
+      <div className="app__content">
+        <Route path="/" component={SpycatSetup} />
+
+        <Switch>
+          <Route exact={true} path={MAIN_MENU} component={MainMenu} />
+          <Route exact={true} path={TEST_SELECTION} component={TestSelection} />
+          <Route exact={true} path={TEST_VALENCES} component={ValencesTest} />
+          <Route
+            exact={true}
+            path={TEST_VALENCES_SETTINGS}
+            component={ValencesTestSettings}
+          />
+          <Route
+            exact={true}
+            path={TEST_PERIODIC_TABLE}
+            component={PeriodicTableTest}
+          />
+          <Route
+            exact={true}
+            path={TEST_PERIODIC_TABLE_SETTINGS}
+            component={PeriodicTableTestSettings}
+          />
+          <Route
+            exact={true}
+            path={MASS_CALCULATOR}
+            component={MassCalculator}
+          />
+          <Route
+            exact={true}
+            path={PERIODIC_TABLE}
+            component={PeriodicTablePage}
+          />
+          <Route exact={true} path={ABOUT} component={About} />
+
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default App;
