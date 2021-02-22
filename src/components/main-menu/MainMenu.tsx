9c8b00b8993fbad50ab2b4c49a87cdf2da10801a
Horus Lugo <horusgoul@gmail.com>
2021-02-22 19:27:30 +0100

    feat(MainMenu): replace atom-icon.png with the Atom component

--- src/components/main-menu/MainMenu.tsx
+++ src/components/main-menu/MainMenu.tsx
@@ -1,36 +1,35 @@
 import * as React from "react";
-import { RouteComponentProps, useHistory, withRouter } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { i18n } from "../../Locale";
 import "./MainMenu.scss";
 import MenuEntry, { IMenuEntry } from "./menu-entry/MenuEntry";
 import RateApp from "../rate-app/RateApp";
-
-type Props = RouteComponentProps<any> & React.Props<any>;
-
-const menuEntries: IMenuEntry[] = [
-  {
-    icon: "test-tube",
-    name: i18n("nav_test"),
-    route: "/tests",
-  },
-  {
-    icon: "periodic-table",
-    name: i18n("periodic_table"),
-    route: "/periodic-table",
-  },
-  {
-    icon: "scale-balance",
-    name: i18n("mass_calculator"),
-    route: "/mass-calculator",
-  },
-  {
-    icon: "info",
-    name: i18n("nav_about"),
-    route: "/about",
-  },
-];
+import Atom from "@/components/atom";
 
 function MainMenu() {
+  const menuEntries: IMenuEntry[] = [
+    {
+      icon: "test-tube",
+      name: i18n("nav_test"),
+      route: "/tests",
+    },
+    {
+      icon: "periodic-table",
+      name: i18n("periodic_table"),
+      route: "/periodic-table",
+    },
+    {
+      icon: "scale-balance",
+      name: i18n("mass_calculator"),
+      route: "/mass-calculator",
+    },
+    {
+      icon: "info",
+      name: i18n("nav_about"),
+      route: "/about",
+    },
+  ];
+
   const history = useHistory();
 
   return (
@@ -38,7 +37,9 @@ function MainMenu() {
       <RateApp />
 
       <div className="main-menu__brand">
-        <div className="main-menu__logo" />
+        <div className="main-menu__atom-container">
+          <Atom weight={24} size={48} />
+        </div>
 
         <div className="main-menu__app-name">{i18n("app_name")}</div>
       </div>
