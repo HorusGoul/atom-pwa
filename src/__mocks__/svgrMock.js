141414c01cf85f3e28b7836ea77a6cb000643f56
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2021-02-26 10:14:47 +1100

    Working smoke test

---
+++ src/__mocks__/svgrMock.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/display-name */
+import * as React from "react";
+
+export default "SvgrURL";
+
+const SvgrMock = React.forwardRef((props, ref) => (
+  <span ref={ref} {...props} />
+));
+export const ReactComponent = SvgrMock;
