4e9b9cb1bd9f2b7d31e13873f838f6481477a59c
Sudhanshu <perfectsudh@gmail.com>
2021-02-27 12:48:21 +0530

    Add test for PeriodicTable
    Refer: #37

---
+++ src/components/periodic-table/PeriodicTable.test.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import PeriodicTable, { IPeriodicTableElement } from "./PeriodicTable";
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import PtElementInfo from "../pt-element/PtElementInfo";
+import ElementManager from "../../ElementManager";
+
+function elementRenderer(atomic: number): IPeriodicTableElement {
+  return {
+    // @ts-ignore fix this
+    component: PtElementInfo,
+    props: {
+      element: ElementManager.getElement(atomic),
+      onClick: undefined,
+    },
+  };
+}
+
+describe("should render the periodic table", () => {
+  beforeAll(() => {
+    ElementManager.loadElements();
+  });
+
+  test("should show a spinner", async () => {
+    const { container } = render(
+      <PeriodicTable elementRenderer={elementRenderer} />
+    );
+    expect(container.querySelector("svg")).toBeInTheDocument();
+  });
+
+  test("should show the periodic table", async () => {
+    const { container } = render(
+      <PeriodicTable elementRenderer={elementRenderer} />
+    );
+    await waitForElementToBeRemoved(container.querySelector("svg"));
+    expect(screen.getByText(/hydrogen/i)).toBeInTheDocument();
+  });
+});
