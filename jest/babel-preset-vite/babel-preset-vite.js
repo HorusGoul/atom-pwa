6af21f8d66123688810f765732aeb9ffccbfa3dd
Michael Peyper <michael.peyper@ailo.io>
2021-03-03 00:33:37 +1100

    Ranamed vite babel plugins from syntax to transform

--- jest/babel-preset-vite/babel-preset-vite.js
+++ jest/babel-preset-vite/babel-preset-vite.js
@@ -2,8 +2,8 @@ module.exports = function (api, opts = {}) {
   const { env = true, glob = true } = opts;
   return {
     plugins: [
-      glob && require("./babel-plugin-syntax-vite-meta-glob.js"),
-      env && require("./babel-plugin-syntax-vite-meta-env.js"),
+      glob && require("./babel-plugin-transform-vite-meta-glob.js"),
+      env && require("./babel-plugin-transform-vite-meta-env.js"),
     ].filter(Boolean),
   };
 };
