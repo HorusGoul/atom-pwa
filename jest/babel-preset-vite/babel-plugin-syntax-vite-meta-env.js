01d4470b129b6b6f19e4d01513e80ee8073d5c5d
Michael Peyper <michael.peyper@ailo.io>
2021-03-02 23:59:56 +1100

    Only derive all import.meta.env overrides from process.env.NODE_ENV

--- jest/babel-preset-vite/babel-plugin-syntax-vite-meta-env.js
+++ jest/babel-preset-vite/babel-plugin-syntax-vite-meta-env.js
@@ -7,19 +7,17 @@ const template = require("@babel/template").default;
  * Add import.meta.env support
  */
 module.exports = function viteMetaEnvBabelPlugin() {
-  const ast = template.ast(`
-  ({
+  const ast = template.ast(`({
     env: {
       ...Object.fromEntries(
-        Object.entries(process.env).filter(([k]) => /^VITE_/.test(k)),
+        Object.entries(process.env).filter(([k]) => /^VITE_/.test(k))
       ),
-      MODE: process.env.MODE || 'test',
+      MODE: process.env.NODE_ENV || 'test',
       NODE_ENV: process.env.NODE_ENV || 'test',
-      DEV: process.env.DEV ? process.env.DEV != false : true,
-      PROD: process.env.PROD ? process.env.PROD != false : false,
+      DEV: process.env.NODE_ENV !== 'production',
+      PROD: process.env.NODE_ENV === 'production'
     },
-  })
-`);
+  })`);
 
   return {
     visitor: {
