e5ea296adfda9bc5e27b65e25acf0ab4bf8e7d36
Horus Lugo <horusgoul@gmail.com>
2021-03-13 12:30:24 +0100

    refactor(Button): convert to hooks

--- src/components/shared/button/Button.tsx
+++ src/components/shared/button/Button.tsx
@@ -1,4 +1,3 @@
-import autobind from "autobind-decorator";
 import classNames from "classnames";
 import * as React from "react";
 import "./Button.scss";
@@ -10,54 +9,52 @@ export interface ButtonProps {
   circle?: boolean;
   link?: string;
   id?: string;
+  children: React.ReactNode;
 }
 
-@autobind
-class Button extends React.Component<ButtonProps> {
-  public render() {
-    const { className, circle, link, id } = this.props;
-    const buttonClass = classNames(
-      "button",
-      {
-        "button--circle": circle,
-      },
-      className
-    );
+function Button({
+  className,
+  circle,
+  link,
+  id,
+  children,
+  onClick,
+}: ButtonProps) {
+  const buttonClass = classNames(
+    "button",
+    {
+      "button--circle": circle,
+    },
+    className
+  );
 
-    if (link) {
-      return (
-        <ButtonBase
-          data-testid={id}
-          data-atom-button
-          component="a"
-          href={link}
-          className={buttonClass}
-          onClick={this.onClick}
-        >
-          {this.props.children}
-        </ButtonBase>
-      );
-    }
-
+  if (link) {
     return (
       <ButtonBase
         data-testid={id}
         data-atom-button
         component="a"
-        role="button"
+        href={link}
         className={buttonClass}
-        onClick={this.onClick}
+        onClick={onClick}
       >
-        {this.props.children}
+        {children}
       </ButtonBase>
     );
   }
 
-  private onClick() {
-    if (this.props.onClick) {
-      this.props.onClick();
-    }
-  }
+  return (
+    <ButtonBase
+      data-testid={id}
+      data-atom-button
+      component="a"
+      role="button"
+      className={buttonClass}
+      onClick={onClick}
+    >
+      {children}
+    </ButtonBase>
+  );
 }
 
 export default Button;
