626f3ab9d85fdad237b412a7219553e3465b83de
Michael Peyper <michael.peyper@ailo.io>
2021-03-19 19:10:20 +1100

    refactor: remove unused import

--- src/components/periodic-table-test/settings/PeriodicTableTestSettings.test.tsx
+++ src/components/periodic-table-test/settings/PeriodicTableTestSettings.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { render, screen, within, waitFor } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
