8b587194b969e51255336a33fdc17c6ff7326dfb
Michael Peyper <michael.peyper@ailo.io>
2021-03-20 00:18:02 +1100

    refactor: extract common hook for test settings

--- src/components/periodic-table-test/hooks/usePeriodicTableTestSettings.ts
+++ src/components/periodic-table-test/hooks/usePeriodicTableTestSettings.ts
@@ -1,52 +1,5 @@
-import * as React from "react";
-import { useSettings, ElementsSettings } from "@/hooks/useSettings";
-import { useElements } from "@/hooks/useElements";
+import { useTestSettings } from "@/hooks/useTestSettings";
 
 export function usePeriodicTableTestSettings() {
-  const { settings, updateSettings } = useSettings();
-  const { elements } = useElements();
-
-  const defaultSetting = React.useMemo(
-    () => ({
-      elements: elements.map((element) => ({
-        atomic: element.atomic,
-        enabled: element.testState.ptTest,
-        stats: {
-          right: 0,
-          times: 0,
-          wrong: 0,
-        },
-      })),
-    }),
-    [elements]
-  );
-
-  const storedSettings = settings.tests.periodicTable;
-
-  const updateTestSettings = React.useCallback(
-    (updateFunction: (settings: ElementsSettings) => void) => {
-      updateSettings((settings) => {
-        updateFunction(settings.tests.periodicTable);
-      });
-    },
-    [updateSettings]
-  );
-
-  const resetSettings = React.useCallback(() => {
-    updateTestSettings((settings) => {
-      settings.elements = defaultSetting.elements;
-    });
-  }, [updateTestSettings, defaultSetting]);
-
-  React.useEffect(() => {
-    if (!settings.tests.periodicTable.elements) {
-      resetSettings();
-    }
-  }, [settings, resetSettings]);
-
-  return {
-    settings: storedSettings.elements ? storedSettings : defaultSetting,
-    updateSettings: updateTestSettings,
-    resetSettings,
-  };
+  return useTestSettings("periodicTable", "ptTest");
 }
--- src/components/valences-test/hooks/useValencesTestSettings.ts
+++ src/components/valences-test/hooks/useValencesTestSettings.ts
@@ -1,52 +1,5 @@
-import * as React from "react";
-import { useSettings, ElementsSettings } from "@/hooks/useSettings";
-import { useElements } from "@/hooks/useElements";
+import { useTestSettings } from "@/hooks/useTestSettings";
 
 export function useValencesTestSettings() {
-  const { settings, updateSettings } = useSettings();
-  const { elements } = useElements();
-
-  const defaultSetting = React.useMemo(
-    () => ({
-      elements: elements.map((element) => ({
-        atomic: element.atomic,
-        enabled: element.testState.valencesTest,
-        stats: {
-          right: 0,
-          times: 0,
-          wrong: 0,
-        },
-      })),
-    }),
-    [elements]
-  );
-
-  const storedSettings = settings.tests.valences;
-
-  const updateTestSettings = React.useCallback(
-    (updateFunction: (settings: ElementsSettings) => void) => {
-      updateSettings((settings) => {
-        updateFunction(settings.tests.valences);
-      });
-    },
-    [updateSettings]
-  );
-
-  const resetSettings = React.useCallback(() => {
-    updateTestSettings((settings) => {
-      settings.elements = defaultSetting.elements;
-    });
-  }, [updateTestSettings, defaultSetting]);
-
-  React.useEffect(() => {
-    if (!settings.tests.valences.elements) {
-      resetSettings();
-    }
-  }, [settings, resetSettings]);
-
-  return {
-    settings: storedSettings.elements ? storedSettings : defaultSetting,
-    updateSettings: updateTestSettings,
-    resetSettings,
-  };
+  return useTestSettings("valences", "valencesTest");
 }
