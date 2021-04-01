11cf3e01b7fc9f37f168338acaa6522f3be43773
Horus Lugo <horusgoul@gmail.com>
2021-04-01 14:44:54 +0200

    feat(Hub): populate sections using the CMS data

--- src/components/hub/Hub.tsx
+++ src/components/hub/Hub.tsx
@@ -1,12 +1,17 @@
+import { useHubCategoryById } from "@/hooks/useHubCategories";
+import { useHubItemById } from "@/hooks/useHubItems";
 import { useTheme } from "@/hooks/useTheme";
 import * as React from "react";
+import { useHistory } from "react-router";
 import Atom from "../atom";
 import HubItem from "./hub-item";
 import HubSection from "./hub-section";
 import styles from "./Hub.module.scss";
+import { HubSectionData, useHub } from "./useHub";
 
 function Hub() {
   const { theme } = useTheme();
+  const { sections } = useHub();
 
   return (
     <div className={styles.hub}>
@@ -54,45 +59,9 @@ function Hub() {
             />
           </HubSection>
 
-          <HubSection title="Tools">
-            <HubItem
-              title="Periodic Table"
-              imageUrl="http://placeimg.com/200/300/periodic-table"
-              colSpan={2}
-            />
-            <HubItem
-              title="Mass Calculator"
-              imageUrl="http://placeimg.com/200/300/mass-calculator"
-            />
-            <HubItem
-              title="Unit Converter"
-              imageUrl="http://placeimg.com/200/300/unit-converter"
-            />
-          </HubSection>
-
-          <HubSection title="Learn">
-            <HubItem
-              title="Quizzes"
-              imageUrl="http://placeimg.com/200/300/quizzes"
-              colSpan={2}
-            />
-            <HubItem
-              title="Courses"
-              imageUrl="http://placeimg.com/300/300/courses"
-              colSpan={2}
-            />
-          </HubSection>
-
-          <HubSection title="Other">
-            <HubItem
-              title="Settings"
-              imageUrl="http://placeimg.com/200/300/settings"
-            />
-            <HubItem
-              title="About"
-              imageUrl="http://placeimg.com/300/300/about"
-            />
-          </HubSection>
+          {sections.map((section) => (
+            <HubSectionWithData key={section.title} {...section} />
+          ))}
         </div>
       </div>
     </div>
@@ -100,3 +69,53 @@ function Hub() {
 }
 
 export default Hub;
+
+function HubSectionWithData({ title, items }: HubSectionData) {
+  return (
+    <HubSection title={title}>
+      {items.map((item) => (
+        <HubItemWithData key={item.item} {...item} />
+      ))}
+    </HubSection>
+  );
+}
+
+type HubItemWithDataProps = HubSectionData["items"][number] & {
+  showCategory?: boolean;
+};
+
+function HubItemWithData({
+  item,
+  colSpan,
+  rowSpan,
+  showCategory = false,
+}: HubItemWithDataProps) {
+  const data = useHubItemById(item);
+  const category = useHubCategoryById(data.category);
+  const history = useHistory();
+
+  function onClick() {
+    if (data.disabled) {
+      return;
+    }
+
+    if (data.href.startsWith("http")) {
+      window.open(data.href);
+      return;
+    }
+
+    history.push(data.href);
+  }
+
+  return (
+    <HubItem
+      title={data.title}
+      disabled={data.disabled}
+      imageUrl={data.imageUrl}
+      category={showCategory ? category.title : undefined}
+      colSpan={colSpan}
+      rowSpan={rowSpan}
+      onClick={onClick}
+    />
+  );
+}
