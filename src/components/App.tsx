27aa3b5c7e1f4d04b37e3baf3b858b0d63844532
Horus Lugo <horusgoul@gmail.com>
2021-04-02 05:18:58 +0200

    refactor: move useElements logic to a Provider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -27,6 +27,7 @@ import ValencesTestSettings from "./valences-test/settings/ValencesTestSettings"
 import ValencesTest from "./valences-test/ValencesTest";
 import Hub from "./hub/Hub";
 import SearchView from "./search-view";
+import { ElementProvider } from "@/contexts/ElementContext";
 
 function App() {
   const { lang, i18n } = useLocale();
@@ -37,68 +38,74 @@ function App() {
   }, [theme]);
 
   return (
-    <div className="app">
-      <Helmet>
-        <html lang={lang} />
+    <ElementProvider>
+      <div className="app">
+        <Helmet>
+          <html lang={lang} />
 
-        <link
-          rel="mask-icon"
-          href="/icons/safari-pinned-tab.svg"
-          color={primaryColor}
-        />
-        <meta name="msapplication-TileColor" content={primaryColor} />
-        <meta name="theme-color" content={primaryColor} />
-
-        <meta name="description" content={i18n("app_description")} />
-
-        <meta name="og:title" content={i18n("app_full_title")} />
-        <meta name="og:description" content={i18n("app_description")} />
-        <meta name="twitter:site" content={i18n("twitter_account")} />
-
-        <title>{i18n("app_full_title")}</title>
-      </Helmet>
-
-      <div className="app__content">
-        <Route path="/" component={SpycatSetup} />
-
-        <Switch>
-          <Route exact={true} path={HUB}>
-            <SearchView />
-            <Hub />
-          </Route>
-          <Route exact={true} path={TEST_SELECTION} component={TestSelection} />
-          <Route exact={true} path={TEST_VALENCES} component={ValencesTest} />
-          <Route
-            exact={true}
-            path={TEST_VALENCES_SETTINGS}
-            component={ValencesTestSettings}
+          <link
+            rel="mask-icon"
+            href="/icons/safari-pinned-tab.svg"
+            color={primaryColor}
           />
-          <Route
-            exact={true}
-            path={TEST_PERIODIC_TABLE}
-            component={PeriodicTableTest}
-          />
-          <Route
-            exact={true}
-            path={TEST_PERIODIC_TABLE_SETTINGS}
-            component={PeriodicTableTestSettings}
-          />
-          <Route
-            exact={true}
-            path={MASS_CALCULATOR}
-            component={MassCalculator}
-          />
-          <Route
-            exact={true}
-            path={PERIODIC_TABLE}
-            component={PeriodicTablePage}
-          />
-          <Route exact={true} path={ABOUT} component={About} />
+          <meta name="msapplication-TileColor" content={primaryColor} />
+          <meta name="theme-color" content={primaryColor} />
 
-          <Route component={NotFound} />
-        </Switch>
+          <meta name="description" content={i18n("app_description")} />
+
+          <meta name="og:title" content={i18n("app_full_title")} />
+          <meta name="og:description" content={i18n("app_description")} />
+          <meta name="twitter:site" content={i18n("twitter_account")} />
+
+          <title>{i18n("app_full_title")}</title>
+        </Helmet>
+
+        <div className="app__content">
+          <Route path="/" component={SpycatSetup} />
+
+          <Switch>
+            <Route exact={true} path={HUB}>
+              <SearchView />
+              <Hub />
+            </Route>
+            <Route
+              exact={true}
+              path={TEST_SELECTION}
+              component={TestSelection}
+            />
+            <Route exact={true} path={TEST_VALENCES} component={ValencesTest} />
+            <Route
+              exact={true}
+              path={TEST_VALENCES_SETTINGS}
+              component={ValencesTestSettings}
+            />
+            <Route
+              exact={true}
+              path={TEST_PERIODIC_TABLE}
+              component={PeriodicTableTest}
+            />
+            <Route
+              exact={true}
+              path={TEST_PERIODIC_TABLE_SETTINGS}
+              component={PeriodicTableTestSettings}
+            />
+            <Route
+              exact={true}
+              path={MASS_CALCULATOR}
+              component={MassCalculator}
+            />
+            <Route
+              exact={true}
+              path={PERIODIC_TABLE}
+              component={PeriodicTablePage}
+            />
+            <Route exact={true} path={ABOUT} component={About} />
+
+            <Route component={NotFound} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </ElementProvider>
   );
 }
 
---
+++ src/contexts/ElementContext.tsx
@@ -0,0 +1,108 @@
+import MiniSearch from "minisearch";
+import { Element } from "@/Element";
+import { useLocale } from "@/hooks/useLocale";
+import * as React from "react";
+
+const elementsMap = import.meta.globEager("../data/elements/*.json") as Record<
+  string,
+  Element
+>;
+
+const elements = Object.values(elementsMap).sort(
+  (elementA, elementB) => elementA.atomic - elementB.atomic
+);
+
+const elementLookup = elements.reduce((prev, next) => {
+  prev[next.atomic] = next;
+
+  return prev;
+}, {} as Record<number, Element>);
+
+function useElementContextLogic() {
+  const { i18n } = useLocale();
+
+  const getElement: (atomic: number) => Element = React.useCallback(
+    (atomic: number) => elementLookup[atomic],
+    []
+  );
+
+  const getElementLocales = React.useCallback(
+    (element: Element): Element => {
+      return {
+        ...element,
+        bondingType:
+          element.bondingType && i18n(`bonding_${element.bondingType}`),
+        group: i18n(`group_${element.group}`),
+        name: i18n(`element_name_${element.name.toLowerCase()}`),
+        standardState:
+          element.standardState &&
+          i18n(`standard_state_${element.standardState}`),
+      };
+    },
+    [i18n]
+  );
+
+  const getLocalizedElement = React.useCallback(
+    (atomic: number) => {
+      const element = getElement(atomic);
+      return getElementLocales(element);
+    },
+    [getElement, getElementLocales]
+  );
+
+  const localizedElements = React.useMemo(() => {
+    return elements.map(getElementLocales);
+  }, [getElementLocales]);
+
+  const searchIndexRef = React.useRef<MiniSearch<Element>>();
+
+  if (!searchIndexRef.current) {
+    searchIndexRef.current = new MiniSearch<Element>({
+      idField: "atomic",
+      fields: [
+        "atomic",
+        "symbol",
+        "name",
+        "atomicMass",
+        "group",
+        "standardState",
+        "bondingType",
+        "electronicConfiguration",
+      ],
+      searchOptions: {
+        fuzzy: 0.5,
+        boost: {
+          atomic: 5,
+          symbol: 5,
+          name: 2,
+          group: 2,
+        },
+        prefix: true,
+      },
+    });
+
+    searchIndexRef.current.addAll(localizedElements);
+  }
+
+  return {
+    elements,
+    getElement,
+    getElementLocales,
+    getLocalizedElement,
+    searchIndex: searchIndexRef.current as MiniSearch<Element>,
+  };
+}
+
+export type ElementContextValue = ReturnType<typeof useElementContextLogic>;
+
+export const ElementContext = React.createContext<ElementContextValue>(
+  {} as ElementContextValue
+);
+
+export function ElementProvider({ children }: { children: React.ReactNode }) {
+  const value = useElementContextLogic();
+
+  return (
+    <ElementContext.Provider value={value}>{children}</ElementContext.Provider>
+  );
+}
