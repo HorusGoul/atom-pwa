33273e935736a6ef52adfa9d4cc9e3d4ed4a692e
Horus Lugo <horusgoul@gmail.com>
2021-06-03 20:50:57 +0200

    fix: conditional call to plausible

--- src/services/spycat.tsx
+++ src/services/spycat.tsx
@@ -15,5 +15,7 @@ export function logEvent<T extends keyof PlausibleEvents>(
   type: T,
   params?: PlausibleEvents[T]
 ) {
-  plausible?.(type, { props: params } as EventOptionsTuple<PlausibleEvents[T]>);
+  window?.plausible(type, { props: params } as EventOptionsTuple<
+    PlausibleEvents[T]
+  >);
 }
