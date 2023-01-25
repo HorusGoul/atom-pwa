831703e598e3b4809b2345c7552f5da0c6bcdd63
Horus Lugo <horusgoul@gmail.com>
2023-01-25 06:03:57 +0100

    Fix type issue

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,5 +1,7 @@
 import { ResizeObserver } from "@juggle/resize-observer";
 
 if (!("ResizeObserver" in window)) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   window.ResizeObserver = ResizeObserver;
 }
