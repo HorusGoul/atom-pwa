28ada1314f0ae28207c15a3d6193698f773aa44e
Horus Lugo <horusgoul@gmail.com>
2023-07-16 03:15:30 +0200

    Collect coverage

--- projects/e2e/src/HubPage.ts
+++ projects/e2e/src/HubPage.ts
@@ -1,15 +1,15 @@
-import { expect, type Locator, type Page } from '@playwright/test';
-
+import type { Page } from "@playwright/test";
+import { expect } from "playwright-test-coverage";
 
 export class HubPage {
   get title() {
-    return this.page.getByLabel('Atom');
+    return this.page.getByLabel("Atom");
   }
 
   constructor(public readonly page: Page) {}
 
   async goto() {
-    await this.page.goto('/');
+    await this.page.goto("/");
   }
 
   async titleIsVisible() {
--- projects/e2e/tests/HubPage.spec.ts
+++ projects/e2e/tests/HubPage.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "playwright-test-coverage";
 import { HubPage } from "@/HubPage";
 
 test("HubPage should have a title", async ({ page }) => {
